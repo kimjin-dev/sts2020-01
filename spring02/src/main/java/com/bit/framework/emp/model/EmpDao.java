33b5ceb4d847e6a1b350f27cd29f44d86f9c1fbd
JinKim <kimjin.dev@gmail.com>
2020-09-07 17:03:39 +0900

    spring jdbc - insert, update

--- spring02/src/main/java/com/bit/framework/emp/model/EmpDao.java
+++ spring02/src/main/java/com/bit/framework/emp/model/EmpDao.java
@@ -13,72 +13,54 @@ import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import com.bit.framework.emp.model.entity.EmpVo;
 
 public class EmpDao extends JdbcDaoSupport {
-		
-		public EmpDao() {
-		}
-		
-		public List<EmpVo> selectAll(){
-			String sql="select * from emp";
-			List<EmpVo> list= new ArrayList<>();
-			
-			try (
-				Connection conn = getConnection();
-				PreparedStatement pstmt= conn.prepareStatement(sql);
-				ResultSet rs = pstmt.executeQuery();
-					){
-				while(rs.next()) {
-					list.add(new EmpVo(
-					rs.getInt("sabun"),rs.getString("name"),rs.getString("sub")
-					,rs.getTimestamp("nalja"),rs.getInt("pay")
-					));
-					
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			return list;
-		}
-		public void insertOne(String name, String sub, int pay) throws SQLException {
-			String sql = "insert into emp (name, sub, nalja, pay) values (?,?,now(),?)";
-			try (
-					Connection conn = getConnection();
-					PreparedStatement pstmt = conn.prepareStatement(sql);
-					){
-					pstmt.setString(1, name);
-					pstmt.setString(2, sub);
-					pstmt.setInt(3, pay);
-					pstmt.executeUpdate();
-			}
-		}
 
-		public EmpVo selectOne(int parseInt) throws SQLException {
-			String sql="select * from emp where sabun=?";
-			try (
-					Connection conn = getConnection();
-					PreparedStatement pstmt = conn.prepareStatement(sql);
-					){
-				pstmt.setInt(1, parseInt);
-				ResultSet rs=pstmt.executeQuery();
-				if(rs.next())
-					return new EmpVo(
-							rs.getInt("sabun"),rs.getString("name"),rs.getString("sub"),
-							rs.getTimestamp("nalja"),rs.getInt("pay")
-							);
-			}
-			return null;
+	public EmpDao() {
+	}
+	
+	public List<EmpVo> selectAll() throws SQLException{
+		String sql="select * from emp";
+		List<EmpVo> list=new ArrayList<>();
+		Connection conn=null;
+		try{
+			conn=getConnection();
+			PreparedStatement pstmt=conn.prepareStatement(sql);
+			ResultSet rs=pstmt.executeQuery();
+			while(rs.next())
+				list.add(new EmpVo(
+						rs.getInt("sabun"),rs.getString("name"),rs.getString("sub")
+						,rs.getTimestamp("nalja"),rs.getInt("pay")
+						));
+		}finally {
+			if(conn!=null)conn.close();
 		}
+		return list;
+	}
 
-		public int updateOne(int sabun, String name, String sub, int pay) throws SQLException {
-			String sql = "update emp set name=?, sub=?, pay=? where sabun=?";
-			try(
-					Connection conn = getConnection();
+	public void insertOne(String name, String sub, int pay) throws SQLException {
+		String sql = "insert into emp (name, sub, nalja, pay) values (?,?,now(),?)";
+		Object[] params=new Object[] {name,sub,pay};
+		getJdbcTemplate().update(sql, params);
+	}
+
+	public EmpVo selectOne(int parseInt) throws SQLException {
+		String sql="select * from emp where sabun=?";
+		try(
+				Connection conn=getConnection();
 				PreparedStatement pstmt=conn.prepareStatement(sql);
-					) {
-				pstmt.setInt(4, sabun);
-				pstmt.setString(1, name);
-				pstmt.setString(2, sub);
-				pstmt.setInt(3, pay);
-				return pstmt.executeUpdate();
-			}
+				){
+			pstmt.setInt(1, parseInt);
+			ResultSet rs=pstmt.executeQuery();
+			if(rs.next())
+				return new EmpVo(
+						rs.getInt("sabun"),rs.getString("name"),rs.getString("sub")
+						,rs.getTimestamp("nalja"),rs.getInt("pay")
+						);
 		}
+		return null;
+	}
+
+	public int updateOne(int sabun, String name, String sub, int pay) throws SQLException {
+		String sql="update emp set name=?,sub=?,pay=? where sabun=?";
+		return getJdbcTemplate().update(sql, new Object[] {name, sub, pay, sabun});
+	}
 }
